b649f655dcbedbd10f4230a68ee4eddcc12d76da
loki <loki@pop-os.localdomain>
2020-05-01 22:13:34 -0500

    adding

--- fahr.c
+++ fahr.c
@@ -1,20 +1,5 @@
 #include <stdio.h>
 
-
-//int main(){
-//  int fahr, celsius;
-//  int lower, upper, step;
-//  lower = 0; upper = 300; step = 20;
-//  fahr = lower;
-//
-//  while(fahr <= upper){
-//    celsius = 5 * (fahr - 32) / 9;
-//    printf("%3d %6d\n",fahr,celsius );
-//    fahr+=step;
-//  }
-//
-//}
-//
 main(){
 	int fahr, celsius;
 	int lower, upper, step;
@@ -26,10 +11,4 @@ main(){
 		fahr += step;
 	
 	}
-
-
-
-
-
-
 }
